ff7af02f63a8b903545621efd2e0a47359ffbfcd
Vignesh Ravindran <vignesh.ravi2211@gmail.com>
2020-12-25 12:06:17 +0530

    changes on package.json

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is a test app with some changes.
+          This is a test app with some changes, tried to change package.json.
         </p>
       </header>
     </div>
